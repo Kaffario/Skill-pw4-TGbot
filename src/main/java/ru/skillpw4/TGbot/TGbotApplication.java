247da799d1973a3b6358ecad2b12f6366d6f45ad
Kaffar <kaffario@gmail.com>
2022-05-13 18:02:37 +0300

    initial commit

---
+++ src/main/java/ru/skillpw4/TGbot/TGbotApplication.java
@@ -0,0 +1,15 @@
+package ru.skillpw4.TGbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGbotApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TGbotApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/ru/skillpw4/TGbot/TGbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillpw4.TGbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
